e6cd2eb7ad00a1767d945a3e21905db4ae9a71c4
Robin Obůrka <robin.oburka@nic.cz>
2014-03-19 10:13:10 +0100

    Add new values and small refactoring

--- luasrc/model/cbi/rainbow/rainbow.lua
+++ luasrc/model/cbi/rainbow/rainbow.lua
@@ -5,6 +5,11 @@ function m.on_after_commit(self)
 	luci.sys.call("/etc/init.d/rainbow restart");
 end
 
+local led_sections = { pwr = "Power", wifi = "Wi-Fi", lan = "LAN ports", wan = "WAN" };
+local colors = { red = "Red (FF0000)", green = "Green (00FF00)", blue = "Blue (0000FF)", white = "White (FFFFFF)", black = "Black (000000)" };
+colors["33FF33"] = "Real white (33FF33)"; --How can I do it better in lua?
+local status_opts = { auto = "Auto", enable = "Enable", disable = "Disable" };
+
 s = m:section(NamedSection, "all", "led", "Set status and color of all LEDs");
 	--s.addremove = true;
 
@@ -12,15 +17,17 @@ o = s:option(Value, "color", "Color");
 	o.default = '33FF33'
 	o.optional = false;
 	o.rmempty = false;
+	for k, v in pairs(colors) do
+		o:value(k, v);
+	end
 
 o = s:option(ListValue, "status", "LEDs status");
 	o.default = 'auto'
 	o.optional = false;
-	o:value('auto');
-	o:value('enable');
-	o:value('disable');
-
-local led_sections = { pwr = "Power", wifi = "Wi-Fi", lan = "LAN ports", wan = "WAN" };
+	o.rmempty = false;
+	for k, v in pairs(status_opts) do
+		o:value(k, v);
+	end
 
 for k, v in pairs(led_sections) do
 	s = m:section(NamedSection, k, "led", "Set status and color of " .. v .. " LED");
@@ -30,13 +37,17 @@ for k, v in pairs(led_sections) do
 		o.default = '33FF33'
 		o.optional = false;
 		o.rmempty = false;
+		for k, v in pairs(colors) do
+			o:value(k, v);
+		end
 
 	o = s:option(ListValue, "status", "LED status");
-		o.default = 'auto'
+		o.default = 'auto';
 		o.optional = false;
-		o:value('auto');
-		o:value('enable');
-		o:value('disable');
+		o.rmempty = false;
+		for k, v in pairs(status_opts) do
+			o:value(k, v);
+		end
 end
 
 return m;
